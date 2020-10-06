22cc5140a1718e8b0340990b361dc9367d21087c
CrispulentCrisps <70386744+CrispulentCrisps@users.noreply.github.com>
2020-10-06 21:03:24 +0100

    Added pause menu

---
+++ Toasterman (Main)/Assets/Game/Scripts/Menu/PauseMenuScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuScript : MonoBehaviour
+{
+    public SceneChange scenechange;
+
+    public GameObject PauseMenuUI;
+    
+    public static bool GameIsPaused = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }   
+    }
+
+    public void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        scenechange.ChangeScene();
+    }
+    
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
